5491465dbe479eb2403aabe2dcca761afeaea962
xloger <phoenix@xloger.com>
2016-01-06 15:02:55 +0800

    添加了标记,改善了布局

---
+++ app/src/main/java/com/xloger/exlink/app/util/ViewTool.java
@@ -0,0 +1,38 @@
+package com.xloger.exlink.app.util;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+/**
+ * Created by xloger on 1月6日.
+ * Author:xloger
+ * Email:phoenix@xloger.com
+ */
+public class ViewTool {
+    /**
+     * 动态设置ListView的高度
+     * @param listView
+     */
+    public static void setListViewHeightBasedOnChildren(ListView listView) {
+        if(listView == null) return;
+
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null) {
+            // pre-condition
+            return;
+        }
+
+        int totalHeight = 0;
+        for (int i = 0; i < listAdapter.getCount(); i++) {
+            View listItem = listAdapter.getView(i, null, listView);
+            listItem.measure(0, 0);
+            totalHeight += listItem.getMeasuredHeight();
+        }
+
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        listView.setLayoutParams(params);
+    }
+}
