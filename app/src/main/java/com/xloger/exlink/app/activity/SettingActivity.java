4c9bb7b0fe0c2d6037ff0c4ba340c7151eefed5f
xloger <phoenix@xloger.com>
2016-02-18 16:39:07 +0800

    完成了隐藏图标功能

--- app/src/main/java/com/xloger/exlink/app/activity/SettingActivity.java
+++ app/src/main/java/com/xloger/exlink/app/activity/SettingActivity.java
@@ -1,7 +1,9 @@
 package com.xloger.exlink.app.activity;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
@@ -12,20 +14,24 @@ import com.xloger.exlink.app.util.FileUtil;
 public class SettingActivity extends Activity implements View.OnClickListener {
 
     private CheckBox debugMode;
+    private CheckBox hiddenIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
+        hiddenIcon = (CheckBox) findViewById(R.id.setting_hidden_icon);
         debugMode = (CheckBox) findViewById(R.id.setting_debug);
 
+        hiddenIcon.setOnClickListener(this);
         debugMode.setOnClickListener(this);
 
         SharedPreferences sp = getSharedPreferences("config", 0);
         boolean isDebugMode = sp.getBoolean("isDebugMode", false);
         debugMode.setChecked(isDebugMode);
-
+        boolean isHiddenIcon = sp.getBoolean("isHiddenIcon", false);
+        hiddenIcon.setChecked(isHiddenIcon);
 
 
     }
@@ -34,14 +40,31 @@ public class SettingActivity extends Activity implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         SharedPreferences sp = getSharedPreferences("config", 0);
-        if (v.getId()==debugMode.getId()){
-            SharedPreferences.Editor edit = sp.edit();
-            edit.putBoolean("isDebugMode",debugMode.isChecked());
-            edit.apply();
+        switch (v.getId()){
+            case R.id.setting_debug:
+                SharedPreferences.Editor edit = sp.edit();
+                edit.putBoolean("isDebugMode",debugMode.isChecked());
+                edit.apply();
 
-            FileUtil fileUtil = FileUtil.getInstance();
-            fileUtil.save(Constant.IS_DEBUG_FILE_NAME, (debugMode.isChecked() + "").getBytes());
-            fileUtil.setReadable(Constant.IS_DEBUG_FILE_NAME);
+                FileUtil fileUtil = FileUtil.getInstance();
+                fileUtil.save(Constant.IS_DEBUG_FILE_NAME, (debugMode.isChecked() + "").getBytes());
+                fileUtil.setReadable(Constant.IS_DEBUG_FILE_NAME);
+                break;
+            case R.id.setting_hidden_icon:
+                SharedPreferences.Editor edit2 = sp.edit();
+                edit2.putBoolean("isHiddenIcon",hiddenIcon.isChecked());
+                edit2.apply();
+                ComponentName componentName=new ComponentName(this,XposedIntoActivity.class);
+                if (hiddenIcon.isChecked()){
+                    PackageManager packageManager = getPackageManager();
+                    packageManager.setComponentEnabledSetting(componentName,PackageManager.COMPONENT_ENABLED_STATE_DISABLED,PackageManager.DONT_KILL_APP);
+                }else {
+                    PackageManager p = getPackageManager();
+                    p.setComponentEnabledSetting(componentName,
+                            PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                            PackageManager.DONT_KILL_APP);
+                }
+                break;
         }
     }
 }
---
+++ app/src/main/java/com/xloger/exlink/app/activity/XposedIntoActivity.java
@@ -0,0 +1,20 @@
+package com.xloger.exlink.app.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by xloger on 2月18日.
+ * Author:xloger
+ * Email:phoenix@xloger.com
+ */
+public class XposedIntoActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent=new Intent(this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
