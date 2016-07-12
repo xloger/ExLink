44b77ac7ca08d181bc161855302bc4ef5ee6a8e7
xloger <phoenix@xloger.com>
2016-07-12 17:23:49 +0800

    Revert "=。=添加了一个 QQ 群信息"
    This reverts commit fb21a1c5d161c95756e68719066566aeed260604.

--- app/src/main/java/com/xloger/exlink/app/activity/AboutActivity.java
+++ app/src/main/java/com/xloger/exlink/app/activity/AboutActivity.java
@@ -1,12 +1,8 @@
 package com.xloger.exlink.app.activity;
 
-import android.content.ClipboardManager;
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.xloger.exlink.app.BuildConfig;
 import com.xloger.exlink.app.R;
@@ -26,14 +22,5 @@ public class AboutActivity extends BaseActivity {
         TextView versions= (TextView) findViewById(R.id.about_version);
         String versionName = BuildConfig.VERSION_NAME;
         versions.setText("版本号："+versionName);
-
-        findViewById(R.id.about_qq).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ClipboardManager cmb = (ClipboardManager) AboutActivity.this.getSystemService(Context.CLIPBOARD_SERVICE);
-                cmb.setText("429861674"); //将内容放入粘贴管理器,在别的地方长按选择"粘贴"即可
-                Toast.makeText(AboutActivity.this,getString(R.string.copy_succeed),Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 }
